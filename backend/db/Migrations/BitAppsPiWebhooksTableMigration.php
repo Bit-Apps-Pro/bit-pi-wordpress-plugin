a0503cc9cee32e7ebda16fb4de2474a09420c822
Arif Uddin <arifunctg@gmail.com>
2024-03-05 16:52:59 +0600

    chore: new migration file added

---
+++ backend/db/Migrations/BitAppsPiWebhooksTableMigration.php
@@ -0,0 +1,34 @@
+<?php
+
+use BitApps\WPDatabase\Blueprint;
+use BitApps\WPDatabase\Schema;
+use BitApps\WPKit\Migration\Migration;
+
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+final class BitAppsPiWebhooksTableMigration extends Migration
+{
+    public function up(): void
+    {
+        Schema::create(
+            'webhooks',
+            function (Blueprint $table): void {
+                $table->id();
+                $table->string('title');
+                $table->bigint('flow_id', 20)->nullable()->unique()->unsigned()->foreign('flows', 'id')->onDelete()->setNull();
+                $table->string('app_slug');
+                $table->string('webhook_slug');
+                $table->longtext('ip_restrictions')->nullable();
+                $table->longtext('details')->nullable();
+                $table->timestamps();
+            }
+        );
+    }
+
+    public function down(): void
+    {
+        Schema::drop('webhooks');
+    }
+}
